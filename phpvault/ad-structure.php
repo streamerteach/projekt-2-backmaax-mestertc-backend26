14d24abf7d9dd309b2c09d3efee89c2b778ddaa3
Axel <axel.backman@hotmail.com>
2026-03-15 21:16:41 +0200

    Stylechange

--- phpvault/ad-structure.php
+++ phpvault/ad-structure.php
@@ -2,35 +2,22 @@
 
 
 <div id="addfield">
-    <div class="subfield">
-        <div class="adds" id="add-1">
-            <a class="add"><img></a>
-        </div>
-        <div class="adds" id="add-2">
-            <a class="add"><img></a>
-        </div>
-        <div class="adds" id="add-3">
-            <a class="add"><img></a>
-        </div>
-        <div class="adds" id="add-4">
-            <a class="add"><img></a>
-        </div>
+    
+    <div class="adds" id="add-1">
+        <img src="../Fileupload/Profilepictures/dennis.jpg" alt="John" style="width:100%">
+        <h1>John Doe</h1>
+        <p class="title">CEO & Founder, Example</p>
+        <p>Harvard University</p>
+        <p><button>Contact</button></p>
+    </div>
+    <div class="adds" id="add-2">
+        <a class="add"><img></a>
+    </div>
+    <div class="adds" id="add-3">
+        <a class="add"><img></a>
     </div>
     
-    <div class="subfield">
-        <div class="adds" id="add-5">
-            <a class="add"><img src="../Fileupload/Profilepictures/dennis.jpg" alt="dennis" width="180" height="202"></a>
-        </div>
-        <div class="adds" id="add-6">
-            <a class="add"><img></a>
-        </div>
-        <div class="adds" id="add-7">
-            <a class="add"><img></a>
-        </div>
-        <div class="adds" id="add-8">
-            <a class="add"><img></a>
-        </div>
-    </div>
+    
 </div>
 <div id="page-turner">
     <div class="turner-element"><img></div>
