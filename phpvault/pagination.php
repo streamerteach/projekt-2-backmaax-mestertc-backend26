2d9a287ab78fc9277332ed02e36a235cdef8c1a6
Axel <axel.backman@hotmail.com>
2026-03-16 19:37:30 +0200

    pagination

---
+++ phpvault/pagination.php
@@ -0,0 +1,28 @@
+<?php
+include "sort-adds.php";
+
+$res_per_page = 5;
+
+$sql = "SELECT COUNT(*) AS total FROM profiles";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$tot_res = $stmt->fetch(PDO::FETCH_ASSOC)['total'];
+
+$pages = ceil($tot_res/$res_per_page);
+
+if (isset($_GET['page'])) {
+    $page = (int) $_GET['page'];
+} else {
+    $page = 1;
+}
+
+$page = max(1, min($page, $pages));
+
+$startpoint = ($page - 1) * $res_per_page;
+
+$stmt = $conn->prepare($baseset);
+$stmt->execute();
+$stmt->bindValue(':startpoint', $startpoint, PDO::PARAM_INT);
+$stmt->bindValue(':res_per_page', $res_per_page, PDO::PARAM_INT);
+$stmt->execute();
+$profiles = $stmt->fetchAll(PDO::FETCH_ASSOC);
